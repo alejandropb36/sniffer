45e2ab653f2a5a7c1d702c1957e60265b990a990
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-10-17 17:07:34 -0500

    Se agrego el codigo del sniffer

---
+++ pypackets.py
@@ -0,0 +1,42 @@
+# import modules
+import socket 
+import struct
+import binascii
+import os
+import pye
+
+# Si el OS en windows
+if os.name == "nt":
+    # Esto nos sirve para referirnos al nuestro propio host
+    HOST = socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
+    s.bind((HOST,0))
+    s.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
+    s.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
+# Si el OS es linux
+else:
+    s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
+
+# ciclo para que no deje de capturar
+while True:
+    
+    # Capture packets from network
+    pkt=s.recvfrom(65565)
+
+    # extract packets with the help of pye.unpack class 
+    unpack=pye.unpack()
+
+    print ("\n\n[+] ------------ Ethernet Header----- [+]")
+
+    # print data on terminal
+    for i in unpack.eth_header(pkt[0][0:14]).iteritems():
+        a,b=i
+        print ("{} : {} | ".format(a,b))
+    print ("\n[+] ------------ IP Header ------------[+]")
+    for i in unpack.ip_header(pkt[0][14:34]).iteritems():
+        a,b=i
+        print ("{} : {} | ".format(a,b))
+    print ("\n[+] ------------ Tcp Header ----------- [+]")
+    for  i in unpack.tcp_header(pkt[0][34:54]).iteritems():
+        a,b=i
+        print ("{} : {} | ".format(a,b))
--- sniffer.py
+++ sniffer.py
@@ -6,9 +6,16 @@
 #import module
 import socket
  
-#create an INET, raw socket
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
- 
+
+HOST = socket.gethostbyname(socket.gethostname())
+# create a raw socket and bind it to the public interface
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+s.bind((HOST, 0))
+# Include IP headers
+s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+# receive all packages
+s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
 # receive a packet
 while True:
 
