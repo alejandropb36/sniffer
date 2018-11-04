88c57dc9ead1c802e6b0748c179a88ddb24d4851
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-11-03 23:59:31 -0600

    Primeras pruebas de scapy

--- sniffer.py
+++ sniffer.py
@@ -2,12 +2,28 @@
 #
 # Simplest Form Of Packet sniffer in python
 # Works On Linux Platform
-#from io import open
+from io import open
 import os
-#import sys
-#from scapy.all import * 
+import sys
+from scapy.all import * 
 from lib.menu import *
 
-menu()
-
-os.system("pause")
+opcionMenu = "1"
+while opcionMenu != "0":
+    menu()
+    opcionMenu = input ("Elige una opcion: ")
+    # if opcionMenu == "1":
+    #     #capturarPaquetes()
+    # elif opcionMenu == "2":
+    #     #importarPaquete()
+    # elif opcionMenu == "3":
+    #     #Trabajando
+    # else:
+    #     print("Adios !!! :V")
+    #     os.system("pause")
+    cant = int(input('\t\t\t\tIngrese la cantidad de paquetes que desea capturar: '))
+    a=sniff(filter="tcp", count=cant)
+    print("",a.nsummary())
+    os.system("pause")
+	#numpck = int(input('\nIngrese el numero del paquete que quiere visualizar: '))
+    #print(hexdump(a[numpck]))
